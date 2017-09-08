a7f4639d9ffb6497110c72f0653b5c8cd6071d2b
Margarita <lifelower@gmail.com>
2017-09-08 09:41:09 +0300

    Финальная версия

---
+++ js/popup.js
@@ -0,0 +1,44 @@
+var link = document.querySelector(".contacts-information .button");
+var popup = document.querySelector(".modal-write-us");
+var close = document.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=client-name]");
+var email = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("name");
+      
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+    name.value = storage;
+    email.focus();
+  } else {
+    name.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("name", name.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+})
